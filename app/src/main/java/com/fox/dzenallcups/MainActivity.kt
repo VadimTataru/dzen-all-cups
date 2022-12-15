d7c233461355de8c7f696ca21ab224d8eea00789
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-15 17:17:35 +0300

    feat: add toast message to buttons

--- app/src/main/java/com/fox/dzenallcups/MainActivity.kt
+++ app/src/main/java/com/fox/dzenallcups/MainActivity.kt
@@ -2,10 +2,27 @@ package com.fox.dzenallcups
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.fox.dzenallcups.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+        binding.btnLatter.setOnClickListener {
+            Toast.makeText(this, "Выбрать позже", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnContinue.setOnClickListener {
+            Toast.makeText(this, "Продолжить", Toast.LENGTH_SHORT).show()
+        }
     }
 }
