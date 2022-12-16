c4c9c379dae2243a200cc4cb9cc14ab14b4a0e02
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-16 08:11:48 +0300

    feat: add mocked data

--- app/src/main/java/com/fox/dzenallcups/data/MockCategory.kt
+++ app/src/main/java/com/fox/dzenallcups/data/MockCategory.kt
@@ -1,5 +1,9 @@
 package com.fox.dzenallcups.data
 
 object MockCategory {
-
+    val categories = mutableListOf(
+        "Юмор", "Еда", "Кино", "Рестораны", "Прогулки",
+        "Политика", "Новости", "Автомобили", "Сериалы",
+        "Рецепты", "Работа", "Отдых", "Спорт"
+    )
 }
